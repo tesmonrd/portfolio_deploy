23af44d0b9da57b991e4e1f20872477556461dcf
Rick Tesmond <tesmonrd@gmail.com>
2016-06-06 15:06:45 -0700

    static loading properly

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "portfolio", "static")
